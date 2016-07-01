2a5ff1b63e59ea49e4c5213fd98819cca0231ea9
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-07-01 17:57:36 +0200

    #48 but please, handle with care

--- src/main/java/de/zalando/zmon/scheduler/ng/StupsOAuthProvider.java
+++ src/main/java/de/zalando/zmon/scheduler/ng/StupsOAuthProvider.java
@@ -1,23 +1,10 @@
 package de.zalando.zmon.scheduler.ng;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Form;
-import org.apache.http.client.fluent.Request;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.zalando.zmon.scheduler.ng.config.StupsOAuthConfig;
 
@@ -27,12 +14,12 @@ import de.zalando.zmon.scheduler.ng.config.StupsOAuthConfig;
 
 public class StupsOAuthProvider {
 
-    private static Logger LOG = LoggerFactory.getLogger(StupsOAuthProvider.class);
+//    private static Logger LOG = LoggerFactory.getLogger(StupsOAuthProvider.class);
+//
+//    private static final String ROBOT_CREDENTIALS_FILE = "user.json";
+//    private static final String CLIENT_CREDENTIALS_FILE = "client.json";
 
-    private static final String ROBOT_CREDENTIALS_FILE = "user.json";
-    private static final String CLIENT_CREDENTIALS_FILE = "client.json";
-
-    private static final ObjectMapper mapper = new ObjectMapper();
+//    private static final ObjectMapper mapper = new ObjectMapper();
 
     private final Map<String, TokenContainer> tokens = new HashMap<>();
 
@@ -56,46 +43,46 @@ public class StupsOAuthProvider {
             return token;
         }
     }
-
-    private static class TokenUpdater implements Runnable {
-        private String key;
-        private String metaFolder;
-        private String uri;
-        private TokenContainer container;
-        private String scopes;
-
-        public TokenUpdater(String uri, String metaFolder, TokenContainer tc) {
-            this.metaFolder = metaFolder;
-            this.uri = uri;
-            this.container = tc;
-            scopes = container.getScopes().stream().filter(x -> x != null).collect(Collectors.joining(" "));
-        }
-
-        @Override
-        public void run() {
-            try {
-                Map<String, String> clientCredentials = mapper.readValue(new File(metaFolder + "/" + CLIENT_CREDENTIALS_FILE), new TypeReference<Map<String, String>>() {
-                });
-                Map<String, String> robotCredentials = mapper.readValue(new File(metaFolder + "/" + ROBOT_CREDENTIALS_FILE), new TypeReference<Map<String, String>>() {
-                });
-
-                final Executor executor = Executor.newInstance();
-                executor.auth(clientCredentials.get("client_id"), clientCredentials.get("client_secret"));
-
-                List<NameValuePair> form = Form.form().add("grant_type", "password")
-                        .add("username", robotCredentials.get("application_username"))
-                        .add("password", robotCredentials.get("application_password"))
-                        .add("scopes", scopes).build();
-
-                final String r = executor.execute(Request.Post(uri).bodyForm(form).useExpectContinue()).returnContent().asString();
-
-                container.updateToken(r, System.currentTimeMillis() + 60 * 60 * 1000);
-
-            } catch (IOException ex) {
-                LOG.error("", ex);
-            }
-        }
-    }
+//
+//    private static class TokenUpdater implements Runnable {
+//        private String key;
+//        private String metaFolder;
+//        private String uri;
+//        private TokenContainer container;
+//        private String scopes;
+//
+//        public TokenUpdater(String uri, String metaFolder, TokenContainer tc) {
+//            this.metaFolder = metaFolder;
+//            this.uri = uri;
+//            this.container = tc;
+//            scopes = container.getScopes().stream().filter(x -> x != null).collect(Collectors.joining(" "));
+//        }
+//
+//        @Override
+//        public void run() {
+//            try {
+//                Map<String, String> clientCredentials = mapper.readValue(new File(metaFolder + "/" + CLIENT_CREDENTIALS_FILE), new TypeReference<Map<String, String>>() {
+//                });
+//                Map<String, String> robotCredentials = mapper.readValue(new File(metaFolder + "/" + ROBOT_CREDENTIALS_FILE), new TypeReference<Map<String, String>>() {
+//                });
+//
+//                final Executor executor = Executor.newInstance();
+//                executor.auth(clientCredentials.get("client_id"), clientCredentials.get("client_secret"));
+//
+//                List<NameValuePair> form = Form.form().add("grant_type", "password")
+//                        .add("username", robotCredentials.get("application_username"))
+//                        .add("password", robotCredentials.get("application_password"))
+//                        .add("scopes", scopes).build();
+//
+//                final String r = executor.execute(Request.Post(uri).bodyForm(form).useExpectContinue()).returnContent().asString();
+//
+//                container.updateToken(r, System.currentTimeMillis() + 60 * 60 * 1000);
+//
+//            } catch (IOException ex) {
+//                LOG.error("", ex);
+//            }
+//        }
+//    }
 
     public String getToken(String scopeIdentifier) {
         return tokens.get(scopeIdentifier).getToken();
