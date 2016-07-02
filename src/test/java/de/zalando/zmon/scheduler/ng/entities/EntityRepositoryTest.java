c8a1dad812618e8a757abe678756650781dab40a
Jan-M <janm81@gmail.com>
2016-07-02 13:25:04 +0200

    Added test for entities added and changed

--- src/test/java/de/zalando/zmon/scheduler/ng/entities/EntityRepositoryTest.java
+++ src/test/java/de/zalando/zmon/scheduler/ng/entities/EntityRepositoryTest.java
@@ -40,4 +40,46 @@ public class EntityRepositoryTest {
 
         assertEquals(1, repository.getCurrentMap().size());
     }
+
+    @Test
+    public void TestNotify() {
+        SchedulerConfig config = new SchedulerConfig();
+        config.setEntityBaseFilterStr("[{\"type\":\"host\"}]");
+
+        EntityAdapterRegistry registry = Mockito.mock(EntityAdapterRegistry.class);
+
+        Entity instance = new Entity("instance-1");
+        instance.addProperty("type", "instance");
+
+        Entity host1 = new Entity("host-1");
+        host1.addProperty("type", "host");
+
+        Entity host1_changed = new Entity("host-1");
+        host1_changed.addProperty("type", "host");
+        host1_changed.addProperty("traffic", "true");
+
+        Entity host2 = new Entity("host-2");
+        host2.addProperty("type", "host");
+
+        List<Entity> entities = asList(instance, host1);
+        List<Entity> entities2 = asList(instance, host1_changed, host2);
+
+        EntityAdapter adapter = Mockito.mock(EntityAdapter.class);
+        when(adapter.getCollection()).thenReturn(entities).thenReturn(entities2);
+
+        when(registry.getSourceNames()).thenReturn(asList("entities"));
+        when(registry.get("entities")).thenReturn(adapter);
+
+        EntityChangeListener listener = Mockito.mock(EntityChangeListener.class);
+
+        EntityRepository repository = new EntityRepository(registry, config);
+        repository.registerListener(listener);
+        assertEquals(1, repository.getCurrentMap().size());
+
+        repository.fill();
+        assertEquals(2, repository.getCurrentMap().size());
+
+        verify(listener).notifyEntityAdd(eq(repository), eq(host2));
+        verify(listener).notifyEntityChange(eq(repository), eq(host1), eq(host1_changed));
+    }
 }
