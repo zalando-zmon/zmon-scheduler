694d3e1ead8e601e0945d431d45e2f6f69697ef5
Rajat parida <rajat.parida@zalando.de>
2019-01-08 16:04:37 +0100

    Separate interface method for batch entity cleanup

---
+++ src/main/java/de/zalando/zmon/scheduler/ng/cleanup/BatchEntityCleanup.java
@@ -0,0 +1,39 @@
+package de.zalando.zmon.scheduler.ng.cleanup;
+
+import de.zalando.zmon.scheduler.ng.entities.Entity;
+import de.zalando.zmon.scheduler.ng.entities.EntityChangeListener;
+import de.zalando.zmon.scheduler.ng.entities.EntityRepository;
+import de.zalando.zmon.scheduler.ng.scheduler.Scheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+public class BatchEntityCleanup implements EntityChangeListener {
+    private Scheduler scheduler;
+    private final static Logger LOG = LoggerFactory.getLogger(BatchEntityCleanup.class);
+
+
+    @Override
+    public void notifyEntityChange(EntityRepository repo, Entity entityOld, Entity entityNew) {
+
+    }
+
+    @Override
+    public void notifyEntityRemove(EntityRepository repo, Entity e) {
+    }
+
+    public BatchEntityCleanup(Scheduler scheduler) {
+        this.scheduler = scheduler;
+    }
+
+    @Override
+    public void notifyBatchEntityRemove(EntityRepository repo, Set<String> removedEntities) {
+        scheduler.scheduleEntityCleanUp(removedEntities);
+    }
+
+    @Override
+    public void notifyEntityAdd(EntityRepository repo, Entity e) {
+
+    }
+}
