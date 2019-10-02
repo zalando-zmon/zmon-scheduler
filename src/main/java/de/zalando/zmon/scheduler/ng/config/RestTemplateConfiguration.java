eb877260a99fde1fef6410a87710fe2f47930bc5
☃ pitr <peter.vernigorov@zalando.de>
2019-10-02 10:51:30 +0200

    accept all content types in RestTemplateConfiguration

--- src/main/java/de/zalando/zmon/scheduler/ng/config/RestTemplateConfiguration.java
+++ src/main/java/de/zalando/zmon/scheduler/ng/config/RestTemplateConfiguration.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Created by jmussler on 11.02.16.
@@ -57,6 +59,7 @@ public class RestTemplateConfiguration {
 
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(mapper);
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
 
         rt.getMessageConverters().clear();
         rt.getMessageConverters().add(converter);
